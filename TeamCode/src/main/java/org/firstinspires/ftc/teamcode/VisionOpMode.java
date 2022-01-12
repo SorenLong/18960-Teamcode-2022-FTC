6bc63f1efb740525917855bbf68d5cf30937acd0
Soren <69611077+SorenLong@users.noreply.github.com>
2022-01-12 17:01:38 -0500

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionOpMode.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.openftc.easyopencv.OpenCvCamera;
+
+@Autonomous(name="AutoWithVision | Intake 2.0")
+public class visionOpMode extends LinearOpMode {
+
+    OpenCvCamera phoneCam;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        int cameraMonitorViewId = hardwareMap.appContext
+                .getResources().getIdentifier("cameraMonitorViewId",
+                        "id", hardwareMap.appContext.getPackageName());
+        phoneCam = OpenCvCameraFactory.getInstance()
+                .createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        SkystoneDetector detector = new SkystoneDetector(telemetry);
+        phoneCam.setPipeline(detector);
+
+    }
